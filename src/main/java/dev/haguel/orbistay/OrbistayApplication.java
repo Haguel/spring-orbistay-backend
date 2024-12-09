2edf7bccad461d32cd9ac69832cc61647b676cc8
GlebHal <hammanov.gleb@gmail.com>
2024-12-09 13:07:28 +0200

    project setup

---
+++ src/main/java/dev/haguel/orbistay/OrbistayApplication.java
@@ -0,0 +1,13 @@
+package dev.haguel.orbistay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbistayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrbistayApplication.class, args);
+    }
+
+}
