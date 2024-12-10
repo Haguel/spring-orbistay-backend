b47f25e552b35a3c23be4bb12442de4c641a6218
GlebHal <hammanov.gleb@gmail.com>
2024-12-10 14:04:53 +0200

    add findAppUserByEmail to user repo

--- src/main/java/dev/haguel/orbistay/repository/AppUserRepository.java
+++ src/main/java/dev/haguel/orbistay/repository/AppUserRepository.java
@@ -3,5 +3,8 @@ package dev.haguel.orbistay.repository;
 import dev.haguel.orbistay.entity.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    Optional<AppUser> findAppUserByEmail(String email);
 }
