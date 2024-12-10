436d11325b0883eb14da1bb15623c9a3636bf8eb
GlebHal <hammanov.gleb@gmail.com>
2024-12-10 14:56:01 +0200

    rename JwtRequestDTO

--- src/main/java/dev/haguel/orbistay/dto/JwtRefreshTokenDTO.java
+++ src/main/java/dev/haguel/orbistay/dto/JwtRefreshTokenDTO.java
@@ -5,8 +5,8 @@ import lombok.Data;
 
 
 @Data
-@Schema(name = "JwtRequestDTO", description = "Data Transfer Object for a JWT request")
-public class JwtRequestDTO {
+@Schema(name = "JwtRefreshTokenDTO", description = "Data Transfer Object for a JWT request")
+public class JwtRefreshTokenDTO {
     @Schema(description = "The JWT refresh token", example = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0QGdtYWlsLmNvbSIsImV4cCI6MTczNTk4MjM3N30.ErYXGf-iEanyTBj6hLTEc_mwQF-C8cGpSEcB8AJZFN")
     private String refreshToken;
 }
