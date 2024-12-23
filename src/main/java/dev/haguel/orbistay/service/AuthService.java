4552c712414d174be1474de1bd34d9edcd6392d3
GlebHal <hammanov.gleb@gmail.com>
2024-12-23 11:22:34 +0200

    add check for username uniqueness to signUp()

--- src/main/java/dev/haguel/orbistay/service/AuthService.java
+++ src/main/java/dev/haguel/orbistay/service/AuthService.java
@@ -11,6 +11,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -40,6 +41,9 @@ public class AuthService {
         } catch (DataIntegrityViolationException exception) {
             if(exception.getMessage().contains("email")) {
                 throw new UniquenessViolationException("Email already exists");
+            }
+            if(exception.getMessage().contains("username")) {
+                throw new UniquenessViolationException("Username already exists");
             }
         }
 
