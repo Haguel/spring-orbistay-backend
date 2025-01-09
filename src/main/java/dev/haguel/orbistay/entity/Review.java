ae351593f4c7ee27fd0fa82e92fd63a651eb5b8b
GlebHal <hammanov.gleb@gmail.com>
2025-01-09 12:19:14 +0200

    adjust Review entity

--- src/main/java/dev/haguel/orbistay/entity/Review.java
+++ src/main/java/dev/haguel/orbistay/entity/Review.java
@@ -1,13 +1,16 @@
 package dev.haguel.orbistay.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import dev.haguel.orbistay.serialization.app_user.ReviewAppUserDeserializer;
 import dev.haguel.orbistay.serialization.app_user.ReviewAppUserSerializer;
+import dev.haguel.orbistay.serialization.hotel.ReviewHotelDeserializer;
+import dev.haguel.orbistay.serialization.hotel.ReviewHotelSerializer;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -25,6 +28,15 @@ public class Review {
     @Column
     private String content;
 
+    @Column
+    private String goodSides; // what was good about the hotel
+
+    @Column
+    private String badSides; // what was bad about the hotel
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
     @Schema(implementation = ReviewAppUserSerializer.InnerSerializer.class)
     @JsonSerialize(using = ReviewAppUserSerializer.class)
     @JsonDeserialize(using = ReviewAppUserDeserializer.class)
@@ -32,8 +44,15 @@ public class Review {
     @JoinColumn(name = "app_user_id", nullable = false)
     private AppUser appUser;
 
+    @Schema(implementation = ReviewHotelSerializer.InnerSerializer.class)
+    @JsonSerialize(using = ReviewHotelSerializer.class)
+    @JsonDeserialize(using = ReviewHotelDeserializer.class)
     @ManyToOne
     @JoinColumn(name = "hotel_id", nullable = false)
-    @JsonIgnore
     private Hotel hotel;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
