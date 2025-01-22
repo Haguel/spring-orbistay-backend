dac7f865f186a8abdc2d38700c5d9bbb0c0cc97d
GlebHal <hammanov.gleb@gmail.com>
2025-01-22 11:43:36 +0200

    WIP

--- src/main/java/dev/haguel/orbistay/config/RedisConfig.java
+++ src/main/java/dev/haguel/orbistay/config/RedisConfig.java
@@ -1,17 +1,46 @@
 package dev.haguel.orbistay.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.data.redis.password}")
+    private String redisPassword;
+
+    @Value("${spring.data.redis.ssl.enabled}")
+    private boolean redisSslEnabled;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        LettuceClientConfiguration clientConfig = null;
+        if(redisSslEnabled) {
+            clientConfig = LettuceClientConfiguration.builder()
+                    .useSsl()
+                    .build();
+        } else {
+            clientConfig = LettuceClientConfiguration.defaultConfiguration();
+        }
+
+        RedisStandaloneConfiguration serverConfig = new RedisStandaloneConfiguration();
+        serverConfig.setHostName(redisHost);
+        serverConfig.setPort(redisPort);
+        serverConfig.setPassword(redisPassword);
+
+        return new LettuceConnectionFactory(serverConfig, clientConfig);
     }
 
     @Bean
