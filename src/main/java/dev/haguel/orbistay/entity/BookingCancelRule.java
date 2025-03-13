af82d944e789af09584ad111023b8e8052d14642
GlebHal <hammanov.gleb@gmail.com>
2025-03-13 10:53:22 +0200

    removed circular dependency

--- src/main/java/dev/haguel/orbistay/entity/BookingCancelRule.java
+++ src/main/java/dev/haguel/orbistay/entity/BookingCancelRule.java
@@ -1,5 +1,6 @@
 package dev.haguel.orbistay.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,5 +25,6 @@ public class BookingCancelRule {
 
     @OneToOne
     @JoinColumn(name = "hotel_id", referencedColumnName = "id")
+    @JsonIgnore
     private Hotel hotel;
 }
