bcbeb54c6a35670c24e2cc455af1fd8ea803a978
GlebHal <hammanov.gleb@gmail.com>
2025-03-15 11:23:05 +0200

    fix test

--- src/test/java/dev/haguel/orbistay/mapper/BaseMapperTestClass.java
+++ src/test/java/dev/haguel/orbistay/mapper/BaseMapperTestClass.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Slf4j
@@ -11,5 +12,6 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @SpringBootTest
 @ActiveProfiles("test")
+@Transactional
 public abstract class BaseMapperTestClass {
 }
